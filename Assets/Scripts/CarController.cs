0fe025f7f2ffcd2249f15a7879151a42efdb65f8
Błażej Sowa <bsowa123@gmail.com>
2017-11-27 20:56:23 +0100

    minimalist

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -5,62 +5,28 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
 
-
 public class CarController : MonoBehaviour
 {
     [SerializeField] private WheelCollider[] m_WheelColliders = new WheelCollider[4];
     [SerializeField] private GameObject[] m_WheelMeshes = new GameObject[4];
-    [SerializeField] private Vector3 m_CentreOfMassOffset;
-    [SerializeField] private float m_MaximumSteerAngle;
-    [Range (0, 1)] [SerializeField] private float m_SteerHelper;
-    // 0 is raw physics , 1 the car will grip in the direction it is facing
-    [Range (0, 1)] [SerializeField] private float m_TractionControl;
-    // 0 is no traction control, 1 is full interference
-    [SerializeField] private float m_FullTorqueOverAllWheels;
-    [SerializeField] private float m_ReverseTorque;
-    [SerializeField] private float m_Downforce = 100f;
-    [SerializeField] private float m_Topspeed = 200;
-    [SerializeField] private float m_SlipLimit;
-    [SerializeField] private float m_BrakeTorque;
+	[SerializeField] private Vector3 m_CentreOfMassOffset = new Vector3(0,0,0);
 
-    private Quaternion[] m_WheelMeshLocalRotations;
-    private Vector3 m_Prevpos, m_Pos;
-    private float m_SteerAngle;
-    private int m_GearNum;
-    private float m_GearFactor;
-    private float m_OldRotation;
-    private float m_CurrentTorque;
+	[SerializeField] private float m_StallTorque = 100f;
+	[SerializeField] private float m_NoLoadSpeed = 100f;
+    [SerializeField] private float m_DownForce = 100f;
+
     private Rigidbody m_Rigidbody;
-    private const float k_ReversingThreshold = 0.01f;
-	private int TotalSamples;
-	private bool isSaving;
-	private Vector3 saved_position;
-	private Quaternion saved_rotation;
-
-    public float CurrentSteerAngle {
-        get { return m_SteerAngle; }
-        set { m_SteerAngle = value; }
-    }
-
-    public float CurrentSpeed{ get { return m_Rigidbody.velocity.magnitude * 2.23693629f; } }
-
-    public float MaxSpeed{ get { return m_Topspeed; } }
-
-    public float Revs { get; private set; }
-
-    public float AccelInput { get; set; }
+	private float m_MaxRPM;
 
     // Use this for initialization
     private void Start ()
     {
-        m_WheelMeshLocalRotations = new Quaternion[4];
-        for (int i = 0; i < 4; i++) {
-            m_WheelMeshLocalRotations [i] = m_WheelMeshes [i].transform.localRotation;
-        }
         m_WheelColliders [0].attachedRigidbody.centerOfMass = m_CentreOfMassOffset;
-
         m_Rigidbody = GetComponent<Rigidbody> ();
-        m_CurrentTorque = m_FullTorqueOverAllWheels - (m_TractionControl * m_FullTorqueOverAllWheels);
+
+		float wheelCircumference = m_WheelColliders [0].radius * Mathf.PI * 2f;
+		m_MaxRPM = (m_NoLoadSpeed / wheelCircumference) * 1000f / 60f;
+		Debug.Log (m_MaxRPM);
     }
 		
 
@@ -76,23 +42,14 @@ public class CarController : MonoBehaviour
 
         //clamp input values
         steering = Mathf.Clamp (steering, -1, 1);
-        AccelInput = accel = Mathf.Clamp (accel, 0, 1);
+        accel = Mathf.Clamp (accel, 0, 1);
         //BrakeInput = footbrake = -1 * Mathf.Clamp (footbrake, -1, 0);
         //handbrake = Mathf.Clamp (handbrake, 0, 1);
 
-        //Set the steer on the front wheels.
-        //Assuming that wheels 0 and 1 are the front wheels.
-        m_SteerAngle = steering * m_MaximumSteerAngle;
-        m_WheelColliders [0].steerAngle = m_SteerAngle;
-        m_WheelColliders [1].steerAngle = m_SteerAngle;
-
-
-
-        SteerHelper ();
         ApplyDrive (accel, steering);
 
         AddDownForce ();
-        TractionControl ();
+        //TractionControl ();
     }
 
 
@@ -101,7 +58,7 @@ public class CarController : MonoBehaviour
 
         float thrustTorque;
 
-        thrustTorque = accel * (m_CurrentTorque / 4f);
+		thrustTorque = accel * m_StallTorque;
         for (int i = 0; i < 4; i++) {
             m_WheelColliders [i].motorTorque = thrustTorque;
         }
@@ -110,34 +67,17 @@ public class CarController : MonoBehaviour
     }
 
 
-    private void SteerHelper ()
-    {
-        for (int i = 0; i < 4; i++) {
-            WheelHit wheelhit;
-            m_WheelColliders [i].GetGroundHit (out wheelhit);
-            if (wheelhit.normal == Vector3.zero)
-                return; // wheels arent on the ground so dont realign the rigidbody velocity
-        }
-
-        // this if is needed to avoid gimbal lock problems that will make the car suddenly shift direction
-        if (Mathf.Abs (m_OldRotation - transform.eulerAngles.y) < 10f) {
-            var turnadjust = (transform.eulerAngles.y - m_OldRotation) * m_SteerHelper;
-            Quaternion velRotation = Quaternion.AngleAxis (turnadjust, Vector3.up);
-            m_Rigidbody.velocity = velRotation * m_Rigidbody.velocity;
-        }
-        m_OldRotation = transform.eulerAngles.y;
-    }
 
 
     // this is used to add more grip in relation to speed
     private void AddDownForce ()
     {
-        m_WheelColliders [0].attachedRigidbody.AddForce (-transform.up * m_Downforce *
+        m_WheelColliders [0].attachedRigidbody.AddForce (-transform.up * m_DownForce *
         m_WheelColliders [0].attachedRigidbody.velocity.magnitude);
     }
 
 
-
+	/*
     // crude traction control that reduces the power to wheel if the car is wheel spinning too much
     private void TractionControl ()
     {
@@ -163,4 +103,5 @@ public class CarController : MonoBehaviour
             }
         }
     }
+	*/
 }
