139b1e57d1da82d3ef43207fd97c6b2861b77088
Błażej Sowa <bsowa123@gmail.com>
2017-12-20 11:10:20 +0100

    added menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void PlayTraining()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void PlayAutonomous() 
+	{
+		SceneManager.LoadScene (2);
+	}
+}
