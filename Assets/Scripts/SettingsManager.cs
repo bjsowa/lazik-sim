a511f06a85a91d7c7d0f0ca838b487aad57fffe0
Błażej Sowa <bsowa123@gmail.com>
2018-01-20 16:43:09 +0100

    add Settings Manager

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Settings
+{
+    public int v1;
+    public int v2;
+
+    public Settings()
+    {
+        v1 = 1;
+        v2 = 2;
+    }
+}
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; private set; }
+
+    public Settings settings;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        Instance.LoadSettings();
+    }
+
+    public void LoadSettings()
+    {
+        try
+        {
+            string jsonData = File.ReadAllText(Application.dataPath + "/settings.json");
+            settings = JsonUtility.FromJson<Settings>(jsonData);
+        }
+        catch (FileNotFoundException)
+        {
+            settings = new Settings();
+            SaveSettings();
+        }
+
+        Debug.Log(settings.v1.ToString() + " " + settings.v2.ToString());
+    }
+
+    public void SaveSettings()
+    {
+        string jsonData = JsonUtility.ToJson(settings, true);
+        File.WriteAllText(Application.dataPath + "/settings.json", jsonData);
+    }
+}
